179a9b360a67eaf2fbef2bf458969d3fc87f2313
Diego Santos <diegosferreiradasilva@outlook.com>
2025-12-09 20:14:58 -0300

    Update calcinhapreta.js

--- calcinhapreta.js
+++ calcinhapreta.js
@@ -7,6 +7,74 @@ const play = document.getElementById('play'); // Const do play.
 // criado uma variavel auxiliar, que irá dizer se está tocando ou não.
 let isPlaying = true;
 
+// criadas variaveis para atulização constante dos nomes das musicas, bandas, img e som.
+const AEncruzilhada = {
+    songName: 'A Encruzilhada',
+    artist: 'Calcinha Preta',
+    File: 'A Encruzilhada'
+}
+
+const AmorDividido = {
+    songName: 'Amor Dividido',
+    artistist: 'Calcinha Preta',
+    File: 'Amor Dividido'
+}
+
+const BabyDoll = {
+    songName: 'Baby Doll',
+    artist: 'Calcinha Preta',
+    File: 'Baby Doll'
+}
+
+const FaçoChover = {
+    songName: 'Faço Chover',
+    artist: 'Calcinha Preta',
+    File: 'Faço Chover'
+}
+
+const FilmeseHistórias = {
+    songName: 'Filmes e Histórias',
+    artist: 'Calcinha Preta',
+    File: 'Filmes e Histórias'
+}
+
+const LoucaPorTi = {
+    songName: 'Louca Por Ti',
+    artist: 'Calcinha Preta',
+    File: 'Louca Por Ti'
+}
+
+const Magica = {
+    songName: 'Magica',
+    artist: 'Calcinha Preta',
+    File: 'Magica'
+}
+
+const Paulinha = {
+    songName: 'Paulinha',
+    artist: 'Calcinha Preta',
+    File: 'Paulinha'
+}
+
+const SemExplicação = {
+    songName: 'Sem Explicação',
+    artist: 'Calcinha Preta',
+    File: 'Sem Explicação'
+}
+
+const TemMaisAlguém = {
+    songName: 'Tem Mais Alguém',
+    artist: 'Calcinha Preta',
+    File: 'Tem Mais Alguém'
+}
+
+const VocêNãoValeNada = {
+    songName: 'Você Não Vale Nada',
+    artist: 'Calcinha Preta',
+    File: 'Você Não Vale Nada'
+}
+
+
 // criado uma funcão de repetição
 function playSong(){
     play.querySelector('.bi').classList.remove('bi-play-circle-fill');
@@ -32,5 +100,13 @@ function playPauseDecider(){
     }
 }
 
+// Mais uma função para adicionar a img, song, nome da banda e musica, conforme for passando.
+function initializeSong(){
+
+}
+
+
 // criado um Even, apatir do click
+initializeSong();
+
 play.addEventListener('click', playPauseDecider);
